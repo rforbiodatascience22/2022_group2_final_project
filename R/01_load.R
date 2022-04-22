fd633415ecb7f41e0f1a287274d1268e36baf84a
prince-ravi-leow-dtu <s220520@dtu.dk>
2022-04-22 10:14:31 +0000

    Improved data loading pipeline

--- R/01_load.R
+++ R/01_load.R
@@ -8,24 +8,22 @@ library("readxl")
 
 
 # Load data ---------------------------------------------------------------
-read_excel(path = "./data/_raw/SD1_excel.xlsx", skip = 1) %>% 
-  write_csv2(file = "./data/SD1_converted.csv") 
 
-SD1_raw <- as_tibble(read_csv2(file = "./data/SD1_converted.csv"))
+SD1_raw <- 
+  read_excel(path = "./data/_raw/SD1_excel.xlsx", skip = 1) %>% 
+  write_csv2(file = "./data/SD1_converted.csv") %>% 
+  as_tibble()
 
-read_excel(path = "./data/_raw/SD2_excel.xlsx", skip = 1) %>% 
-  write_csv2(file = "./data/SD2_converted.csv") 
-
-SD2_raw <- as_tibble(read_csv2(file = "./data/SD2_converted.csv"))
+SD2_raw <-
+  read_excel(path = "./data/_raw/SD2_excel.xlsx", skip = 1) %>% 
+  write_csv2(file = "./data/SD2_converted.csv") %>% 
+  as_tibble()
 
 # Wrangle data ------------------------------------------------------------
 SD1_clean <-
   SD1_raw %>%
   separate(`Taxonomic classification`, into = c("k", "domain", "p", "phylum", "c", "className", "o", "order", "f", "family", "g", "genus")) %>%
   select(-k, -p, -c, -o, -g, -f)
-
-rm(SD_cle)
-
 
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data,
