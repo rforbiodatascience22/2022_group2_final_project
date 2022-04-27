ca995196db28b617ce991361d095c6a81995b479
Adi-krishna <s212940@student.dtu.dk>
2022-04-27 13:14:30 +0000

    supplementary data 2: fig 4: plots

--- R/Adi_work.R
+++ R/Adi_work.R
@@ -24,6 +24,7 @@ SD2_raw <-
 #source(file = "R/99_project_functions.R")
 
 
+
 #day 4 :
 
 #plots for supplementary data 2----------------------------
@@ -32,6 +33,59 @@ SD2_raw <-
 #create data for fig 4 
 SD2_data <- SD2_clean %>% 
   pivot_longer(names_to = "ISSlocation", values_to = "ValueL", COLA1:N1C)
+
+#plot1 (have to figure out the y axis scale )
+plot1 <- SD2_data %>%
+  filter(domain == "Archaea" | 
+           domain == "Bacteria" | 
+           domain == "Eukaryota" | 
+           domain == "Viruses")  %>%
+  ggplot(mapping = aes(x = ISSlocation,
+                       y = ValueL, 
+                       fill = domain)) + 
+  geom_bar(position = "fill",
+           stat="identity") +
+  #ylim(0, 15) +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x= "ISS Locations",
+       y = "Abundance (TSS)") +
+  theme(axis.text.x = element_blank())
+#scale_fill_manual(values = c("", "", "", ""))  #how to identify color names to use here?
+plot1
+
+#note : have to figure out how to filter all phylum and genus and the y axis scale range
+
+#plot2   
+plot2 <- SD2_data %>%
+  ggplot(mapping = aes(x = ISSlocation,
+                       y = ValueL, 
+                       fill = phylum)) + 
+  geom_bar(position = "fill",
+           stat="identity") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x= "ISS Locations",
+       y = "Abundance (TSS)") +
+  theme(axis.text.x = element_blank())
+
+plot2
+
+
+#plot 3 
+
+plot3 <- SD2_data %>%
+  filter(Genus == c("Streptococcus", "Corynebacterium", "Lactobacillus", "Acinetobacter", "Staphylococcus", "NA")) %>%
+  ggplot(mapping = aes(x = ISSlocation,
+                       y = ValueL, 
+                       fill = genus)) + 
+  geom_bar(position = "fill",
+           stat="identity") +
+  scale_y_continuous(labels = scales::percent) +
+  labs(x= "ISS Locations",
+       y = "Abundance (TSS)") +
+  theme(axis.text.x = element_blank())
+#scale_fill_manual(values = c("", "", "", "")) 
+
+plot3
 
 
 
