143bb371becf1279b298d6d0034536c7b7fdbcd5
prince-ravi-leow-dtu <s220520@dtu.dk>
2022-05-02 09:12:45 +0000

    Prince contribution figure 4

--- R/Adi_work.R
+++ R/Adi_work.R
@@ -62,3 +62,30 @@ plot1 <- SD2_data %>%
 #scale_fill_manual(values = c("", "", "", ""))
 
 plot1
+
+#Prince 
+SD2_raw <-
+  read_excel(path = "./data/SD2_excel.xlsx", skip = 1, na = c(""," ","NA"))%>% 
+  write_csv2(file = "./data/SD2_converted.csv") %>% 
+  as_tibble()
+
+SD2_table <- SD2_raw %>% 
+  mutate (COLA1 = sqrt(COLA1/sum(COLA1)),
+          COLB1 = sqrt(COLB1/sum(COLB1)),
+          N2A = sqrt(N2A/sum(N2A)),
+          N2B = sqrt(N2B/sum(N2B)),
+          N3C1 = sqrt(N3C1/sum(N3C1)),
+          N1C = sqrt(N1C/sum(N1C)))
+
+SD2_top_40 <- SD2_table %>% 
+  pivot_longer(names_to = "ISSlocation", values_to = "ValueL", COLA1:N1C) %>% 
+  arrange(desc(ValueL)) %>% 
+  top_n(500)
+
+
+SD2_top_40 %>%
+  ggplot(mapping = aes(x = ISSlocation,
+                       y = ValueL, 
+                       fill = phylum)) + 
+  geom_bar(position = "stack",
+           stat="identity")
