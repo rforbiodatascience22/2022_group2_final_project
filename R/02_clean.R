faae3e34a71f8a8f0a78b8ed6706a8b33b0da0f1
Adi-krishna <s212940@student.dtu.dk>
2022-05-02 09:54:17 +0000

    updating 02_clean.R

--- R/02_clean.R
+++ R/02_clean.R
@@ -23,6 +23,23 @@ SD2_raw <-
 # Wrangle data ------------------------------------------------------------
 
 
+# data wrangling and visualization supplementary data 2
+SD2_clean <- SD2_raw %>% 
+  mutate (COLA1 = sqrt(COLA1/sum(COLA1)),
+          COLB1 = sqrt(COLB1/sum(COLB1)),
+          N2A = sqrt(N2A/sum(N2A)),
+          N2B = sqrt(N2B/sum(N2B)),
+          N3C1 = sqrt(N3C1/sum(N3C1)),
+          N1C = sqrt(N1C/sum(N1C)))
+
+#data for figure 4: supplementary data 2
+SD2_table <- SD2_clean %>% 
+  pivot_longer(names_to = "ISSlocation", values_to = "ValueL", COLA1:N1C) %>% 
+  arrange(desc(ValueL)) %>% 
+  top_n(100)
+
+
+
 
 # Write data --------------------------------------------------------------
 write_tsv(x = my_data_clean,
